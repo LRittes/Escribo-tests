e4a7665b54860d7d48aff84bfa14dcb028f9f3c7
LRittes <leandro_rittes@hotmail.com>
2022-08-16 12:41:33 -0300

    finish tests

--- et-01/test/et_01_test.dart
+++ et-01/test/et_01_test.dart
@@ -1,8 +1,13 @@
-import 'package:et_01/et_01.dart';
+import '../bin/et_01.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    // expect(calculate(3), 42);
-  });
+  List<int> numbers = [3, 5, 10, 11];
+  List<int> expected = [0, 3, 23, 33];
+  for (var idx = 0; idx < numbers.length; idx++) {
+    test('Deve retornar ${expected[idx]} com entrada de dado ${numbers[idx]}\n',
+        () {
+      expect(calculate(3), equals(0));
+    });
+  }
 }
